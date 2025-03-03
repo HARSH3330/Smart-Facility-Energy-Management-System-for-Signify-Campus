fd7f0f4182c89caa26ea0dea1cc8d9f0e2667c93
HARSH3330 <harshagarwal244@gmail.com>
2025-03-03 13:00:24 +0530

    Add files via upload

---
+++ smart-facility/src/main/java/com/example/smart_facility/SmartFacilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_facility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFacilityApplication.class, args);
+	}
+
+}
